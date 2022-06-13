7118ac7dfc0770b894a0298cd6b5b0938bb13d5c
cortesrmzcau <cesar.a.cortes.94@gmail.com>
2022-06-13 13:13:30 -0500

    12 Manejo de ambientes

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -4,12 +4,14 @@ import { retry } from 'rxjs/operators';
 
 import { CreateProductDTO, Product, UpdateProductDTO } from '../models/product.model';
 
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = '/api/products';
+  private apiUrl = `${environment.API_URL}/api/products`;
 
   constructor(
     private http: HttpClient
